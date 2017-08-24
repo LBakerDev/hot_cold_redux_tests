d8702c135d442e6008499c94f9fabd076768c3dd
londonbaker4@gmail.com <londonbaker4@gmail.com>
2017-08-23 22:13:42 -0400

    add'l tests

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -6,5 +6,16 @@ import Game from './game';
 describe('<Game />', () => {
     it('Renders without crashing', () => {
         shallow(<Game />);
-    })
-})
+    });
+
+    it('Dispatches toggleInfoModal when the close button is clicked', () => {
+        const dispatch = jest.fn();
+        const wrapper = shallow(<InfoModal dispatch={dispatch} />)
+        wrapper.find('.close').simulate('click', {
+            preventDefault() {}
+        });
+        expect(dispatch).toHaveBeenCalled();
+        expect(dispatch.mock.calls[0][0]).toEqual(toggleInfoModal());
+    });
+});
+
